a14bf53eabeef33847cb9b6106d599e80e017006
Hypario <fabienduterte@orange.fr>
2019-09-16 21:36:21 +0200

    framework can now check if route exist in views

--- src/Framework/TwigExtensions/RouterTwigExtension.php
+++ src/Framework/TwigExtensions/RouterTwigExtension.php
@@ -23,13 +23,40 @@ class RouterTwigExtension extends AbstractExtension
     {
         return [
             new TwigFunction('path', [$this, 'pathFor']),
-            new TwigFunction('is_subpath', [$this, 'is_subpath'])
+            new TwigFunction('is_subpath', [$this, 'isSubPath']),
+            new TwigFunction('has', [$this, 'hasRoute'])
         ];
     }
 
+    /**
+     * generate a path from given name
+     *
+     * @param string $name
+     * @param array $params
+     * @param array $queryParams
+     * @return string
+     * @throws \Exception
+     */
     public function pathFor(string $name, array $params = [], array $queryParams = [])
     {
         return $this->router->getPath($name, $params, $queryParams);
+    }
+
+    /**
+     * Check if named route exist
+     *
+     * @param string $name
+     * @return bool
+     */
+    public function hasRoute(string $name) {
+        return $this->router->hasRoute($name);
+    }
+
+    public function isSubPath(string $path): bool
+    {
+        $uri = $_SERVER['REQUEST_URI'] ?? '/';
+        $expectedUri = $this->router->getPath($path);
+        return strpos($uri, $expectedUri) !== false;
     }
 
 }
