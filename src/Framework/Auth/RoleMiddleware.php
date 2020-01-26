9ca0192d982d27c8ebb9125455777d813ab7b138
Hypario <fabienduterte@mailfence.com>
2020-01-26 17:31:47 +0100

    Added auth in framework

---
+++ src/Framework/Auth/RoleMiddleware.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace App\Framework\Auth;
+
+
+use Framework\Auth\Auth;
+use Framework\Auth\ForbiddenException;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class RoleMiddleware implements MiddlewareInterface
+{
+
+    /**
+     * @var Auth
+     */
+    private $auth;
+    /**
+     * @var string
+     */
+    private $role;
+
+    public function __construct(Auth $auth, string $role)
+    {
+        $this->auth = $auth;
+        $this->role = $role;
+    }
+
+    /**
+     * @inheritDoc
+     * @throws ForbiddenException
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $user = $this->auth->getUser();
+        if (is_null($user) || !in_array($this->role, $user->getRoles())) {
+            throw new ForbiddenException();
+        }
+        return $handler->handle($request);
+    }
+}
